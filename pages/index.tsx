4f3ffed3efde4ce053bb74edf2323e126e0ec136
ingjjaa2 <jonavi1305@gmail.com>
2021-09-29 15:13:59 -0400

    second commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Index =(props:any)=>{
+  return (
+    <div>
+      Home
+    </div>
+  );
+}
+
+
+export default Index;
